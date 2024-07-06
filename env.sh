34d51ba20697f021e192cc4367adc17b2b14693c
bondarev <javeoff@yandex.ru>
2024-07-06 22:44:01 +0400

    feat: update config

--- env.sh
+++
@@ -1,2 +0,0 @@
-export CONFIG_DIR=~/shared/shared-configs/shared-dotfiles/.config
-export DOTFILES_DIR=$CONFIG_DIR/..
--- sh/install.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zx
-import { acceptCmd } from './acceptCmd.mjs'
-
-await acceptCmd('Install zsh?', 'make zsh-install')
-await acceptCmd('Install brew?', 'make brew-install')
